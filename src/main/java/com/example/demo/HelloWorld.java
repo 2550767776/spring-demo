7e64ea9749d5fe8447478f4b496a4ae8c8cc5ccd
ylc <2550767776@qq.com>
2019-05-02 22:30:48 +0800

    init3

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,7 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloWorld {
 
-    @RequestMapping("/index/{name}")
+    @GetMapping("/index/{name}")
     @ResponseBody
     public String index(@PathVariable String name) {
         if (null == name) {
